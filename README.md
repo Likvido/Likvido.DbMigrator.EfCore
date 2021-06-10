7227eb654d939a2acf9cc5f6e249e31f6d0e80e7
Alexander Boychenko <boichenko.alexander@gmail.com>
2021-06-10 19:20:11 +0700

    Initial commit (#1)
    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Likvido.DbMigrator.EfCore
+# Likvido.DbMigrator.EfCore [![GitHub Workflow Status](https://img.shields.io/github/workflow/status/likvido/Likvido.DbMigrator.EfCore/Publish%20to%20nuget)](https://github.com/Likvido/Likvido.DbMigrator.EfCore/actions?query=workflow%3A%22Publish+to+nuget%22) [![Nuget](https://img.shields.io/nuget/v/Likvido.DbMigrator.EfCore)](https://www.nuget.org/packages/Likvido.DbMigrator.EfCore/)
+Should be used to simplify the process of applying migrations
+# Usage
+## Run as a job
+```
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    private readonly ContextFactory<AppDbContext> _contextFactory;
+
+    public ApplicationDbContextFactory(ContextFactory<AppDbContext> contextFactory)
+    {
+        _contextFactory = contextFactory;
+    }
+
+    public ApplicationDbContextFactory()
+    {
+        _contextFactory = new ContextFactory<AppDbContext>("../ProjectWithAppSettings");
+    }
+
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        return _contextFactory.CreateDbContext("ProjectWithMigrations");
+    }
+}
+
+static async Task Main(string[] args)
+{
+    await new MigrationsRunner<ApplicationDbContext, ApplicationDbContextFactory>("role-for-telemetry").Run();
+}
+```
+## Run migrations from Package Manager Console
+Since we often have more than one context we should be specific. Default project should be a project that contains the specified context.
+```
+Update-Database -Verbose -Context AppDbContext 
+```
+## Run migrations from console
+Should be executed in a project folder where the specified context defined
+```
+dotnet ef database update --context AppDbContext
+```
---
+++ src/Likvido.DbMigrator.EfCore/MigrationsRunner.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Likvido.Extensions.Logging;
+using Likvido.Payables.Services.Data;
+using Likvido.Robots;
+using Microsoft.ApplicationInsights;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace Likvido.DbMigrator
+{
+    public class MigrationsRunner<TContext, TContextFactory>
+        where TContextFactory : IDesignTimeDbContextFactory<TContext>
+        where TContext: DbContext
+    {
+        private ILogger<MigrationsRunner<TContext, TContextFactory>>? _logger;
+        private readonly string _role;
+
+        public MigrationsRunner(string role)
+        {
+            _role = role;
+        }
+
+        public async Task Run(string operationName = "db-migration")
+        {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            try
+            {
+                await DoRun(operationName);
+            }
+            catch (Exception e)
+            {
+                Log.Fatal("Set up failed", e);
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        protected virtual async Task DoRun(string operationName)
+        {
+            await new Robot(_role)
+                .BuildOperation(operationName)
+                .SetFunc(Execute)
+                .SetReportError((message, ex) =>
+                {
+                    if (_logger != null)
+                    {
+                        _logger.LogCritical(ex, message);
+                    }
+                    else
+                    {
+                        Log.Fatal(message, ex);
+                    }
+                })
+                .SetConfigureServices(Configure)
+                .SetOnServiceProviderBuild(sp =>
+                    _logger = sp.GetRequiredService<ILogger<MigrationsRunner<TContext, TContextFactory>>>())
+                .SetPostExecute(LoggerShutdown)
+                .Run();
+        }
+
+        protected virtual Task LoggerShutdown()
+        {
+            Log.CloseAndFlush();
+            return Task.CompletedTask;
+        }
+
+        protected virtual void Configure(IConfiguration configuration, IServiceCollection services)
+        {
+            services.AddSingleton<ContextFactory<TContext>>();
+            services.UseSerilog((serviceProvider, loggerConfiguration) =>
+            {
+                var telemetryClient = serviceProvider.GetRequiredService<TelemetryClient>();
+                loggerConfiguration
+                    .ReadFrom.Configuration(configuration)
+                    .Enrich.WithMachineName()
+                    .WriteTo.ApplicationInsights(telemetryClient, TelemetryConverter.Traces)
+                    .WriteTo.Console()
+                    .Enrich.FromLogContext();
+            });
+        }
+
+        protected virtual async Task Execute(IServiceProvider services)
+        {
+            var contextFactory = ActivatorUtilities.CreateInstance<TContextFactory>(services);
+            var context = contextFactory.CreateDbContext(new string[0]);
+            var db = context.Database;
+
+            var pendingMigrations = (await db.GetPendingMigrationsAsync()).ToList();
+            pendingMigrations.Insert(0, "Pending migrations:");
+            _logger.LogInformation(string.Join($"{Environment.NewLine}", pendingMigrations.ToArray()));
+
+            await db.MigrateAsync();
+        }
+    }
+}
